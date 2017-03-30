33b8d1a09a31aa46b88957c7a55296167318e553
Julie Moussu <julie.moussu@savoirfairelinux.com>
2017-03-30 12:27:02 -0400

    Update tests

--- partner_tracking/tests/test_res_partner.py
+++ partner_tracking/tests/test_res_partner.py
@@ -2,42 +2,73 @@
 # © 2017 Savoir-faire Linux
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from openerp.tests import TransactionCase
+from openerp.tests import SavepointCase
+from openerp.exceptions import Warning
 
 
-class TestResPartner(TransactionCase):
+class TestResPartner(SavepointCase):
 
-    def setUp(self):
-        super(TestResPartner, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        super(TestResPartner, cls).setUpClass()
 
-        self.partner = self.env['res.partner'].create({
+        cls.normal_user = cls.env['res.users'].create({
+            'name': 'Normal User',
+            'login': 'normaluserlogin',
+            'email': 'normaluser@test.com',
+        })
+
+        cls.controller_user = cls.env['res.users'].create({
+            'name': 'Controller User',
+            'login': 'controlleruserlogin',
+            'email': 'controlleruser@test.com',
+            'groups_id': [(4, cls.env.ref(
+                'partner_tracking.group_partner_validation'
+            ).id)],
+        })
+
+        cls.partner = cls.env['res.partner'].create({
              'name': 'Partner Test',
         })
 
-        self.test_user = self.env['res.users'].create({
-            'name': 'Test User',
-            'login': 'testlogin',
-            'email': 'test@test.com',
-        })
+        cls.normal_user_partner = cls.normal_user.partner_id
 
-    def test_state_switching(self):
-        self.test_user.partner_id.state = 'controlled'
+    def test_write_updating_preferences(self):
+        """
+        Test the case of the user changing his preferences.
+        """
+        self.normal_user_partner.state = 'controlled'
+        self.assertEqual(self.normal_user_partner.state, 'controlled')
         ctx = {'action': self.env.ref('base.action_res_users_my').id}
-        self.test_user.sudo(self.test_user).with_context(params=ctx).write({
-            'email': 'changetest@test.com',
-        })  # test the case of the user updating his preferences
-        self.assertEqual(self.test_user.partner_id.state, 'controlled')
+        self.normal_user.sudo(self.normal_user).with_context(params=ctx).write(
+            {'email': 'changetest@test.com'}
+        )
+        self.assertEqual(self.normal_user_partner.state, 'controlled')
+
+    def test_write_normal_user(self):
+        """
+        Test the case of a user that is not part of the validation group
+        updating a partner and trying to change the state of a partner.
+        """
         self.assertEqual(self.partner.state, 'controlled')
-        self.partner.sudo(self.test_user).write({
+        self.partner.sudo(self.normal_user).write({
             'name': 'Partner Name Change Test',
-        })  # test the case in which the user is not in the validation group
+        })
         self.assertEqual(self.partner.state, 'pending')
-        self.partner.action_set_controlled()
+        with self.assertRaises(Warning):
+            self.partner.sudo(self.normal_user).action_set_controlled()
+
+    def test_write_controller_user(self):
+        """
+        Test the case of user that is part of the validation group updating a
+        partner and validating a partner.
+        """
         self.assertEqual(self.partner.state, 'controlled')
-        self.env.ref(
-            'partner_tracking.group_partner_validation'
-        ).users = [(4, self.test_user.id)]
-        self.partner.sudo(self.test_user).write({
+        self.partner.sudo(self.controller_user).write({
             'name': 'Partner Name Change Test 2',
-        })  # test the case in which the user is in the validation group
+        })
+        self.assertEqual(self.partner.state, 'controlled')
+        self.partner.state = 'pending'
+        self.assertEqual(self.partner.state, 'pending')
+        self.partner.sudo(self.controller_user).action_set_controlled()
         self.assertEqual(self.partner.state, 'controlled')
