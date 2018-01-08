f9b7106d2891c73262ad12499508fa6654dbbdf0
Yasmine El Mrini <yasmine.elmrini@savoirfairelinux.com>
2018-01-08 15:38:00 -0500

    Remove product_reference

--- product_reference/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2017 Savoir-faire Linux
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import (
-    product_template_reference_type,
-    product_template_reference,
-    product_template,
-)
