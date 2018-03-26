33682a436a62ba7027d49e6279cd50522173c3f3
David Dufresne <dufresne.david.2@gmail.com>
2018-03-26 12:01:40 -0400

    Fix pylint

--- partner_duplicate_multi_phone/models/res_partner_duplicate.py
+++ partner_duplicate_multi_phone/models/res_partner_duplicate.py
@@ -3,7 +3,7 @@
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import models
 
 
 class ResPartnerDuplicateWithPhoneComparisons(models.Model):
