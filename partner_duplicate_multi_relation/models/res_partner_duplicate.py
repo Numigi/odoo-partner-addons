a5b8bf2e5683f1975bf90f7b3a29bd27687144b9
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2018-04-17 16:38:59 -0400

    Add module partner_duplicate_multi_relation (#14)
    * Add module partner_duplicate_multi_relation

---
+++ partner_duplicate_multi_relation/models/res_partner_duplicate.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class ResPartnerDuplicateWithRelationPropagation(models.Model):
+    """Propagate relations from the preserved partner to the archived partner."""
+
+    _inherit = 'res.partner.duplicate'
+
+    def merge_partners(self):
+        res = super().merge_partners()
+        self._process_relations()
+        return res
+
+    def _process_relations(self):
+        """Process relations for which the left partner is partner to archive.
+
+        If a similar relation exists on the preserved partner, then it is deleted.
+        Otherwise, the relation is updated to point on the preserved partner.
+
+        2 equivalent relations are relations that have the same partners and
+        the same type.
+        """
+        self._process_left_relations()
+        self._process_right_relations()
+
+    def _process_left_relations(self):
+        """Process relations for which the left partner is partner to archive."""
+        relation_cls = self.env['res.partner.relation'].with_context(active_test=False)
+
+        relations_left = relation_cls.search([
+            ('left_partner_id', '=', self.partner_archived_id.id),
+            ('right_partner_id', '!=', self.partner_preserved_id.id),
+        ])
+
+        for relation in relations_left:
+            equivalent_relation = relation_cls.search([
+                ('left_partner_id', '=', self.partner_preserved_id.id),
+                ('type_id', '=', relation.type_id.id),
+            ])
+
+            if equivalent_relation:
+                relation.unlink()
+            else:
+                relation.left_partner_id = self.partner_preserved_id.id
+
+    def _process_right_relations(self):
+        """Process relations for which the right partner is partner to archive."""
+        relation_cls = self.env['res.partner.relation'].with_context(active_test=False)
+
+        relations_right = relation_cls.search([
+            ('right_partner_id', '=', self.partner_archived_id.id),
+            ('left_partner_id', '!=', self.partner_preserved_id.id),
+        ])
+
+        for relation in relations_right:
+            equivalent_relation = relation_cls.search([
+                ('right_partner_id', '=', self.partner_preserved_id.id),
+                ('type_id', '=', relation.type_id.id),
+            ])
+
+            if equivalent_relation:
+                relation.unlink()
+            else:
+                relation.right_partner_id = self.partner_preserved_id.id
---
+++ partner_duplicate_multi_relation/wizard/base_partner_merge_automatic_wizard.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class MergePartnerAutomaticPreventPropagateRelations(models.TransientModel):
+    """Prevent base.partner.merge from propagating relations."""
+
+    _inherit = 'base.partner.merge.automatic.wizard'
+
+    def _get_fk_on(self, table):
+        """Remove partner relations from the list of table to update.
+
+        The method _get_fk_on is used to define which foreign keys must be redirected
+        from the archived partner to the preserved partner.
+
+        We remove partner relations so that relations are managed by the
+        method merge_partners of res.partner.duplicate.
+        """
+        foreign_keys = super()._get_fk_on(table)
+        return [r for r in foreign_keys if 'res_partner_relation' not in r[0]]
