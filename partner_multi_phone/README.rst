168a0905d48fd56bb167407a5a084164d1f810ba
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2018-04-20 15:18:00 -0400

    [11.0] Allow searching any phone number easily from the list view (#20)
    * [11.0] Allow searching any phone number easily from the list view
    
    * Codacy issues

--- partner_multi_phone/README.rst
+++ partner_multi_phone/README.rst
@@ -13,6 +13,16 @@ The module adds a field `Main Phone`.
 This field allows to select which phone type should be prioritised for the partner.
 If the field Main Phone is filled, the selected phone type becomes mandatory.
 
+Searching A Partner
+-------------------
+The module allows to easily find a partner given a phone number.
+
+In the list view of partners, you may enter a fragment of the phone number of your
+partner. The system will attempt to match any of the 4 phone fields.
+
+This features works as well with or without the non-digit caracters of the phone number.
+Searching 4504623434 should return a partner with the number +1 450-462-3434.
+
 Contributors
 ------------
 * Numigi (tm) and all its contributors (https://bit.ly/numigiens)
--- partner_multi_phone/models/res_partner.py
+++ partner_multi_phone/models/res_partner.py
@@ -6,6 +6,8 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
+PHONE_FIELDS = ('phone', 'mobile', 'phone_home', 'phone_other')
+
 
 class ResPartnerWithExtraPhones(models.Model):
 
@@ -31,3 +33,42 @@ class ResPartnerWithExtraPhones(models.Model):
     def _onchange_phone_other_validation(self):
         if self.phone_other:
             self.phone_other = self.phone_format(self.phone_other)
+
+
+class ResPartnerWithPhoneSearch(models.Model):
+
+    _inherit = 'res.partner'
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        """Allow searching partners from a phone number."""
+        is_phone_search = (
+            self._context.get('search_partner_multi_phone') and
+            args and isinstance(args[0], (list, tuple)) and args[0][0] == 'phone'
+        )
+        if is_phone_search:
+            phone_number = args[0][2]
+            args = self._get_multi_phone_search_domain(phone_number)
+
+        return super().search(args, offset=offset, limit=limit, order=order, count=count)
+
+    @staticmethod
+    def _get_multi_phone_search_domain(phone_number):
+        """Get a domain expression for searching a phone number from any phone field.
+
+        The returned domain allows to search a phone given only a series of digits.
+
+        For example, searching 4504623434 would allow to find partners with the number
+        +1 450-462-3434.
+
+        :param phone_number: the phone number to search
+        :return: a search domain
+        """
+        phone_expression = '%'.join(c for c in phone_number)
+        return [
+            '|', '|', '|',
+            ('phone', 'ilike', phone_expression),
+            ('mobile', 'ilike', phone_expression),
+            ('phone_home', 'ilike', phone_expression),
+            ('phone_other', 'ilike', phone_expression),
+        ]
