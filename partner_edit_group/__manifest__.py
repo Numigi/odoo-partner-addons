8fa3e021f9b0bd53bacece25f790c4b09cbb742c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-05-31 13:19:28 -0400

    Add module partner_edit_group

---
+++ partner_edit_group/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Partner Edit Group',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://www.numigi.com',
+    'license': 'LGPL-3',
+    'category': 'Partner Management',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+}
