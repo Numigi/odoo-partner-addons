3b2812841e9ecff15911b8d76d568b6f0b190c1c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-12-29 09:58:01 -0500

    TA#29776 Add module partner_filters_simplified

---
+++ partner_filters_simplified/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Partner Filters Simplified',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Simplify Customers and Suppliers filters on partners',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner.xml',
+    ],
+    'installable': True,
+}
