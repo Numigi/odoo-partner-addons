e0ed70ce7d6995c0335accdb2f46c94dd9e62877
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-07-05 10:22:39 -0400

    TA#37660 Add module partner_account_manager (#80)
    * Add module partner_account_manager
    
    * TA#37660 make account manager a commercial field
    
    * TA#37660 add README

---
+++ partner_account_manager/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Partner Account Manager",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Define an account manager on partners",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "installable": True,
+}
