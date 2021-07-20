e1f218d17fdebdd417b7fe0741fc49fbd76fc01d
Gabriel Gosselin Roberge <56849645+gabgosrob@users.noreply.github.com>
2021-07-20 11:24:33 -0400

    add partner_industry_tree_view module

---
+++ partner_industry_tree_view/README.rst
@@ -0,0 +1,23 @@
+Partner Industry Tree View
+==========================
+This module adds the industry sector field to the partner tree view.
+
+.. contents:: Table of Contents
+
+Usage
+-----
+In the partner tree view, you can now see the partner city next to their email.
+
+.. image:: static/description/city.png
+
+Configuration
+-------------
+No configuration is required after installation.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ partner_industry_tree_view/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Industry Tree View",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "AGPL-3",
+    "category": "Partner Management",
+    "summary": "Adds the industry sector to the partner tree view.",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
