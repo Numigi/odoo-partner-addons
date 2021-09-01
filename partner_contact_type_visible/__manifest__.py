dc693f6d28d3b89e2e0c46602850d16d5bb936b7
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-09-01 10:15:48 -0400

    Update __manifest__.py

--- partner_contact_type_visible/__manifest__.py
+++ partner_contact_type_visible/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Partner Contact Type Visible',
-    'version': '1.1.0',
+    'version': '1.2.0',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://bit.ly/numigi-com',
