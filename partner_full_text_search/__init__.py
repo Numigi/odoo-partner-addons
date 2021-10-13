8bb1a444b409eec6ad6d6216ff0175cb8f6182f9
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-10-13 11:43:28 -0400

    TA#36707 add module partner_full_text_search (#86)
    * TA#36707 add module partner_full_text_search

---
+++ partner_full_text_search/__init__.py
@@ -0,0 +1,5 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
+from .init_hook import post_init_hook
---
+++ partner_full_text_search/init_hook.py
@@ -0,0 +1,14 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import logging
+from odoo import api, SUPERUSER_ID
+
+logger = logging.getLogger(__name__)
+
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    logger.info('Updating partner full text index')
+    partners = env['res.partner'].with_context(active_test=False).search([])
+    partners.update_full_text()
