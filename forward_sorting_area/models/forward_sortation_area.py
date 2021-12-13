c73f1bcc1636ae10d7949398ec2e20cfbabd43b5
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-12-13 11:54:00 -0500

    TA#40833 add form views to FSA and territories

--- forward_sorting_area/models/forward_sortation_area.py
+++ forward_sorting_area/models/forward_sortation_area.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # © 2017 Savoir-faire Linux
-# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2018 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, fields, models
@@ -8,29 +7,57 @@ from odoo import api, fields, models
 
 class ForwardSortationAera(models.Model):
 
-    _name = 'forward.sortation.area'
-    _description = 'Forward Sorting Area'
+    _name = "forward.sortation.area"
+    _description = "Forward Sorting Area"
+    _inherit = "mail.thread"
 
-    name = fields.Char('Name', required=True)
-    sector = fields.Char('Sector')
-    province = fields.Char('Province')
-    latitude = fields.Char('Latitude')
-    longitude = fields.Char('Longitude')
-    active = fields.Boolean('Active', default=True)
+    name = fields.Char(
+        "Name",
+        required=True,
+        track_visibility="onchange",
+    )
+    sector = fields.Char(
+        "Sector",
+        track_visibility="onchange",
+    )
+    province = fields.Char(
+        "Province",
+        track_visibility="onchange",
+    )
+    latitude = fields.Char(
+        "Latitude",
+        track_visibility="onchange",
+    )
+    longitude = fields.Char(
+        "Longitude",
+        track_visibility="onchange",
+    )
+    active = fields.Boolean(
+        "Active",
+        default=True,
+        track_visibility="onchange",
+    )
 
     territory_ids = fields.Many2many(
-        'res.territory', 'res_territory_fsa_rel', 'fsa_id', 'territory_id',
-        string='Territories')
+        "res.territory",
+        "res_territory_fsa_rel",
+        "fsa_id",
+        "territory_id",
+        string="Territories",
+        track_visibility="onchange",
+    )
 
-    partner_ids = fields.One2many('res.partner', 'fsa_id', string='Partners')
+    partner_ids = fields.One2many("res.partner", "fsa_id", string="Partners")
 
     @api.multi
     def write(self, vals):
         res = super(ForwardSortationAera, self).write(vals)
-        if 'name' in vals:
-            self.mapped('partner_ids')._compute_fsa_id()
-            self.env['res.partner'].search([
-                ('zip', 'ilike', vals['name'] + '%'),
-            ])._compute_fsa_id()
+        if "name" in vals:
+            self.mapped("partner_ids")._compute_fsa_id()
+            self.env["res.partner"].search(
+                [
+                    ("zip", "ilike", vals["name"] + "%"),
+                ]
+            )._compute_fsa_id()
 
         return res
--- forward_sorting_area/models/res_territory.py
+++ forward_sorting_area/models/res_territory.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # © 2017 Savoir-faire Linux
-# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2018 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
@@ -8,20 +7,35 @@ from odoo import fields, models
 
 class ResTerritory(models.Model):
 
-    _name = 'res.territory'
-    _description = 'Territory'
+    _name = "res.territory"
+    _description = "Territory"
+    _inherit = "mail.thread"
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char(
+        "Name",
+        required=True,
+        track_visibility="onchange",
+    )
 
     fsa_ids = fields.Many2many(
-        'forward.sortation.area',
-        'res_territory_fsa_rel', 'territory_id', 'fsa_id',
-        string='FSA')
+        "forward.sortation.area",
+        "res_territory_fsa_rel",
+        "territory_id",
+        "fsa_id",
+        string="FSA",
+        track_visibility="onchange",
+    )
 
     partner_ids = fields.Many2many(
-        string='Partners',
-        comodel_name='res.partner',
-        relation='rel_territory_partner')
+        string="Partners", comodel_name="res.partner", relation="rel_territory_partner"
+    )
 
-    color = fields.Integer('Color Index')
-    active = fields.Boolean('Active', default=True)
+    color = fields.Integer(
+        "Color Index",
+        track_visibility="onchange",
+    )
+    active = fields.Boolean(
+        "Active",
+        default=True,
+        track_visibility="onchange",
+    )
