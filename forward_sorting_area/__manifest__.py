1030b06be0702b0d71b60113202023779d102160
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-12-13 12:05:13 -0500

    TA#40833 add form views to FSA and territories (#90)

--- forward_sorting_area/__manifest__.py
+++ forward_sorting_area/__manifest__.py
@@ -1,27 +1,27 @@
-# -*- coding: utf-8 -*-
 # © 2017 Savoir-faire Linux
-# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2018 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Forward Sorting Areas',
-    'version': '1.1.0',
-    'author': 'Savoir-faire Linux',
-    'maintainer': 'Numigi',
-    'website': 'https://bit.ly/numigi-com',
-    'license': 'LGPL-3',
-    'category': 'Partner Management',
-    'summary': 'Add territories and forward sorting areas (FSA)',
-    'depends': [
-        'base_setup',
-        'contacts',
-        'web_list_column_width',
+    "name": "Forward Sorting Areas",
+    "version": "1.1.0",
+    "author": "Savoir-faire Linux",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Partner Management",
+    "summary": "Add territories and forward sorting areas (FSA)",
+    "depends": [
+        "base_setup",
+        "contacts",
+        "web_list_column_width",
+        "mail_improved_tracking_value",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/forward_sortation_area.xml',
-        'views/res_partner.xml',
-        'views/res_territory.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/forward_sortation_area.xml",
+        "views/res_partner.xml",
+        "views/res_territory.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- forward_sorting_area/models/res_partner.py
+++ forward_sorting_area/models/res_partner.py
@@ -1,28 +1,29 @@
-# -*- coding: utf-8 -*-
 # © 2017 Savoir-faire Linux
-# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2018 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, fields, models
 
 
-class ResPartnerWithFSA(models.Model):
+class ResPartner(models.Model):
 
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     fsa_id = fields.Many2one(
-        'forward.sortation.area', string='FSA',
-        compute='_compute_fsa_id', store=True)
+        "forward.sortation.area", string="FSA", compute="_compute_fsa_id", store=True
+    )
 
     territory_ids = fields.Many2many(
-        'res.territory', string='Territories', related='fsa_id.territory_ids',
-        readonly=True)
+        "res.territory",
+        string="Territories",
+        related="fsa_id.territory_ids",
+        readonly=True,
+    )
 
-    @api.depends('zip')
+    @api.depends("zip")
     def _compute_fsa_id(self):
         areas = {
-            a.name.lower(): a for a in
-            self.env['forward.sortation.area'].search([])
+            a.name.lower(): a for a in self.env["forward.sortation.area"].search([])
         }
         for record in self:
             if record.zip:
