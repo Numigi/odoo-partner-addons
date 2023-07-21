0f5f0f17548926ecde1617612a46768632a2c77e
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-07-21 18:00:05 +0300

    TA#52117 [ADD] partner_autocomplete_disable_sync_cron : disable cron (#113)
    * TA#52117 [ADD] partner_autocomplete_disable_sync_cron : disable cron

---
+++ partner_autocomplete_disable_sync_cron/README.rst
@@ -0,0 +1,11 @@
+Partner Autocomplete Disable Sync Cron
+======================================
+Disable the autocompletion partner cron.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ partner_autocomplete_disable_sync_cron/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Partner Autocomplete Disable Sync Cron",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "category": "Other",
+    "summary": "Disable the Partner autocomplete cron",
+    "depends": [
+        "partner_autocomplete",
+    ],
+    "data": [
+        "data/ir_cron_data.xml",
+    ],
+    "installable": True,
+}
