c9df1cea514b0f92e765fb66cb5f3d14cec4b0d4
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2023-08-01 09:48:13 +0100

    TA#55883 [ADD] update module version

--- partner_affiliate_extended/__manifest__.py
+++ partner_affiliate_extended/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Partner Affiliate Extended',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://bit.ly/numigi-com',
