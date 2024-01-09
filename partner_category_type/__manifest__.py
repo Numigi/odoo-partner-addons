d2f6d83331b4dba47af8972f0dc31090d8af034f
Majda EL MARIOULI <majda.elmariouli@numigi.com>
2024-01-08 23:10:21 -0500

    TA#59433 [UPD] Bump module version

--- partner_category_type/__manifest__.py
+++ partner_category_type/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Partner Category Type',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'author': 'Savoir-faire Linux',
     'maintainer': 'Numigi',
     'website': 'https://bit.ly/numigi-com',
