2837d108d0ea3acc2dfaa349f5c6f71e46fb8a47
majouda <majdaelmariouli@gmail.com>
2024-06-25 10:24:49 -0400

    TA#66731 [ADD][16.0] Initiate branch

--- partner_duplicate_mgmt/__init__.py
+++
@@ -1,6 +0,0 @@
-# © 2017-2018 Savoir-faire Linux
-# © 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import models, wizard
-from .init_hook import update_partners_indexed_name
--- partner_duplicate_mgmt/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# © 2017-2018 Savoir-faire Linux
-# © 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import (
-    test_res_partner,
-    test_res_partner_duplicate,
-)
