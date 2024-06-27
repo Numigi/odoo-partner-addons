b80efabc1b59c4f1b394fb3e6f4bca926da098c4
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-06-27 18:21:24 +0100

    [16.0][MIG]  partner_autocomplete_disable

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -11,7 +11,7 @@
     "category": "Other",
     "summary": "Install all addons required for testing.",
     "depends": [
-        "contacts",
+        "partner_autocomplete_disable",
     ],
     "installable": True,
 }
