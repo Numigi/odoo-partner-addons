4a73996fa375d4cb86ee1555ecc3f28ec9ca1fbe
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-06-27 18:42:14 +0100

    [16.0][MIG]  partner_autocomplete_disable

--- partner_autocomplete_disable/tests/test_res_partner.py
+++ partner_autocomplete_disable/tests/test_res_partner.py
@@ -2,11 +2,11 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from ddt import ddt, data
-from unittest import mock
+from unittest.mock import patch
 from odoo.tests import common
 
 
-@ddt
+
 class TestResPartner(common.TransactionCase):
     def setUp(self):
         super().setUp()
@@ -25,11 +25,16 @@ class TestResPartner(common.TransactionCase):
             assert self.partner.autocomplete(query) == []
             assert mocked.call_count == 0
 
-    @data("konvergo", "odoo", "something", "google.com", "")
-    def test_enrich_company(self, name):
-        with mock.patch("odoo.addons.iap.jsonrpc") as mocked:
-            assert self.partner.enrich_company(name, 123, "U12345678") == {}
-            assert mocked.call_count == 0
+    @patch('odoo.addons.iap.jsonrpc')
+    def test_enrich_company(self, mock_jsonrpc):
+        # Setup the mock to return an empty dictionary
+        mock_jsonrpc.return_value = {}
+
+        names = ["konvergo", "odoo", "something", "google.com", ""]
+        for name in names:
+            with self.subTest(name=name):
+                result = self.env['res.partner'].enrich_company(name, 123, "U12345678")
+                self.assertEqual(result, {})
 
     @data(
         "U12345678",
