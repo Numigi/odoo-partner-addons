6b6d9f82f566c58adb3b44801bb934107c1268c5
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-07-02 12:11:42 -0400

    Revert "TA#60956 [CHG] Dependencies and cron option (#137)" (#141)
    This reverts commit f49c7e6a8a041cd274a344220fe8caf3987154c7.

--- partner_key_date/__manifest__.py
+++ partner_key_date/__manifest__.py
@@ -4,24 +4,24 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "Partner Key Dates",
-    "version": "1.1.1",
-    "author": "Savoir-faire Linux",
-    "maintainer": "Numigi",
-    "website": "https://bit.ly/numigi-com",
-    "license": "LGPL-3",
-    "category": "Partner Management",
-    "depends": ["mail", "contacts", "queue_job_cron"],
-    "data": [
-        "data/email_template.xml",
-        "data/ir_cron.xml",
-        "security/ir.model.access.csv",
-        "views/res_partner.xml",
-        "views/res_partner_date.xml",
-        "views/res_partner_date_type.xml",
+    'name': 'Partner Key Dates',
+    'version': '1.1.0',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Partner Management',
+    'depends': ['mail', 'contacts'],
+    'data': [
+        'data/email_template.xml',
+        'data/ir_cron.xml',
+        'security/ir.model.access.csv',
+        'views/res_partner.xml',
+        'views/res_partner_date.xml',
+        'views/res_partner_date_type.xml',
     ],
-    "installable": True,
-    "external_dependencies": {
-        "python": ["freezegun"],
+    'installable': True,
+    'external_dependencies': {
+        'python': ['freezegun'],
     },
 }
