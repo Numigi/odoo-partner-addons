36d473614d8e7ad20386e5eab67128e4b4ea50ec
Rivo Lalaina <rivo2302@gmail.com>
2025-07-01 09:41:53 +0300

    [14.0][ÆDD] partner_read_access_actions

---
+++ partner_read_access_actions/__manifest__.py
@@ -0,0 +1,16 @@
+# copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Partner Read Access Actions',
+    'version': "14.0.1.0.0",
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'stock, ',
+    'summary': 'Additional read access actions for partners',
+    'depends': ['contacts', 'base_extended_security'],
+    'data': [],
+    'installable': True,
+}
